30a470ac6fc0b2ec7df9ed25bb24cc0d5a84fd01
jmiramont <jmiramont@uner.edu.ar>
2022-02-16 14:42:11 +0100

    New methods added and testing functionality

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,59 +1,49 @@
 import numpy as np
-import subprocess
 import importlib
 from methods import *
 import pytest
 from benchmark_demo.SignalBank import SignalBank
 from benchmark_demo.utilstf import add_snr
 
+#-------------------------------------------------------------------------------------------------------
+""" This collects all the methods in the folder/ module "methods" and make a global list of them."""
 modules = dir()
 modules = [mod_name for mod_name in modules if mod_name.startswith('method_')]
-# print(modules)
-global list_of_methods
+global list_of_methods # Use with caution.
 list_of_methods = list()    
 for mod_name in modules:
     mod = importlib.import_module('methods.' + mod_name)
     # print(mod)
-    method = getattr(mod, 'return_method_instance')
+    method = getattr(mod, 'instantiate_method')
     list_of_methods.append(method())
 
+
+#-------------------------------------------------------------------------------------------------------
+""" Here start the tests"""
+
+# Generate dummy inputs for testing the methods.
 @pytest.fixture
 def dummy_input():
     N = 256
-    dummy_input = np.zeros((10,N))
+    dummy_input = np.zeros((5,N))
     signal_bank = SignalBank(N)
-    for i in range(10):
+    for i in range(5):
         dummy_input[i,:] = add_snr(signal_bank.linear_chirp(),15)
     return dummy_input
 
-
-def test_methods_outputs(dummy_input):
+# Test the shape of the outputs
+def test_methods_outputs_shape(dummy_input):
         for method_instance in list_of_methods:
             method_id = method_instance.get_method_id()
+            print(method_id)
             output = method_instance.method(dummy_input, params=None)
-            assert (output.shape == dummy_input.shape), method_id +' output should be the same shape as input.'
-
-# a_dummy_input = dummy_input()
-# test_methods_outputs(a_dummy_input)
-# test_methods_outputs(, )
-
-# La idea acá es RECHAZAR un método que no cumpla con lo que tiene que cumplir para que no haya
-# problemas corriendo el benchmark
-
-# ¿Cómo testear CADA método? Ahora puedo listar los métodos, y correrlos. El tema es poder aplicar un
-# test para cada uno. Se supone que pytest corre una función "test_*" y debería ser una función con esa
-# firma.
-
-# Después de esto hay que ver, por un lado, que las acciones de GitHub corran los tests y por otro
-# que CORRAN EL BENCHMARK.
+            assert (output.shape == dummy_input.shape), method_id +' output should have the same shape as input.'
 
 
-
-# def test_output_shape(dummy_input):
-
-
-# def test_output_type(dummy_input):
-#     output = method_HT(dummy_input, params=None)
-#     assert (type(output) is np.ndarray)
-
-
+# Test the type of the outputs
+def test_methods_outputs_type(dummy_input):
+        for method_instance in list_of_methods:
+            method_id = method_instance.get_method_id()
+            print(method_id)
+            output = method_instance.method(dummy_input, params=None)
+            assert (type(output) is np.ndarray), method_id +' output should be numpy.ndarray.'
