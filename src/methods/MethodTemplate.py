1bd3c1f0460e827461b706c6ed66faa63a12857d
jmiramont <jmiramont@uner.edu.ar>
2022-02-18 12:55:41 +0100

    More changes in the file-tree.

---
+++ src/methods/MethodTemplate.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class MethodTemplate(ABC):
+
+    def __init__(self, task, id):
+        self.task = task
+        self.id = id
+
+    @abstractmethod
+    def method(self):
+        ...
+
+    def get_parameters(self):
+        return [None,]
+
+    def get_method_id(self):
+        return self.id
+    
+    def get_task(self):
+        return self.task
+
+        
