d224c466c2632fa27bd7a3401a1750352994152c
jmiramont <jmiramont@uner.edu.ar>
2022-03-21 13:36:18 +0100

    Updating detection task...

--- run_this_benchmark_detection.py
+++ run_this_benchmark_detection.py
@@ -1,4 +1,3 @@
-print('Loading benchmark modules...')
 import importlib
 from methods import *
 from methods.MethodTemplate import MethodTemplate as MethodTemplate
@@ -9,10 +8,8 @@ modules = dir()
 modules = [mod_name for mod_name in modules if mod_name.startswith('method_')]
 global list_of_methods # Use with caution.
 
-print('Collecting methods...')
 list_of_methods = list()    
 for mod_name in modules:
-    print(mod_name)
     mod = importlib.import_module('methods.' + mod_name)
     classes_in_mod = inspect.getmembers(mod, inspect.isclass)
     for a_class in classes_in_mod:
@@ -21,7 +18,6 @@ for mod_name in modules:
         if class_parent == MethodTemplate:
             list_of_methods.append(method_class())
 
-print('Finished.')
 
 from benchmark_demo.Benchmark import *
 import numpy as np
@@ -31,37 +27,52 @@ from benchmark_demo.ResultsInterpreter import ResultsInterpreter
 dictionary_of_methods = dict()
 dictionary_of_parameters = dict()
 
-# Select only those methods for detection.
+# Select only those methods for denoising.
+
 for method_instance in list_of_methods:
-    if method_instance.task == 'detection':
+    if method_instance.task == 'denoising':
         method_id = method_instance.id
         dictionary_of_methods[method_id] = method_instance.method
         dictionary_of_parameters[method_id] = method_instance.get_parameters()
 
-SNRin = [10, 20]
+SNRin = [0, 10, 20, 30]
 
 # Standard test:
-signal_names = ['LinearChirp',] # Single-component signals
-                # 'McCrossingChirps',                       # Crossing-components  
-                # 'McHarmonic','McPureTones',]               # Multi-Component Harmonic signals  
-                # 'McModulatedTones','McDoubleCosChirp',    # Multi-Component Non-Harmonic  
-                # 'McSyntheticMixture','McSyntheticMixture2',
-                # 'HermiteFunction','HermiteElipse',        # Hermite type signals  
-                # 'ToneDumped','ToneSharpAttack',           # Dumped and Sharps attacks  
+# signal_names = ['LinearChirp', 'CosChirp', 'ExpChirp',      # Single-component signals
+#                 'ToneDumped','ToneSharpAttack',           # Dumped and Sharps attacks
+#                 'McCrossingChirps',                         # Crossing-components  
+#                 'McHarmonic','McPureTones',                 # Multi-Component Harmonic signals  
+#                 'McModulatedTones','McDoubleCosChirp',     # Multi-Component Non-Harmonic  
+#                 'McSyntheticMixture','McSyntheticMixture2',
+#                 'HermiteFunction',                          # Hermite type signals  
+#                 'McTripleImpulse2',
+#                 'McOnOffTones']                           # Modes that born and die
+
+
+# Standard test:
+signal_names = ['CosChirp', 'ExpChirp',      # Single-component signals
+                'ToneSharpAttack',           # Dumped and Sharps attacks
+                'McCrossingChirps',                         # Crossing-components  
+                'McMultiLinear', 'McCosPlusTone',                 # Multi-Component signals  
+                'McMultiCos',     # Multi-Component Non-Harmonic  
+                'McSyntheticMixture2','McSyntheticMixture3',
+                'HermiteFunction',                          # Hermite type signals  
+                'McImpulses','McTripleImpulse']
                 # 'McOnOffTones']                           # Modes that born and die
 
+
+
 if __name__ == "__main__":
-    print('Starting benchmark...')
     np.random.seed(0) 
-    my_benchmark = Benchmark(task = 'detection',
+    my_benchmark = Benchmark(task = 'denoising',
                             methods = dictionary_of_methods,
-                            N = 256,
+                            N = 512,
                             parameters = dictionary_of_parameters, 
                             SNRin = SNRin,
                             using_signals=signal_names, 
-                            repetitions=200,
+                            repetitions = 30,
                             verbosity=4,
-                            parallelize=False)
+                            parallelize=5)
 
     start = time.time()
     my_results = my_benchmark.run_test() # Run the test. my_results is a nested dictionary with the results for each of the variables of the simulation.
@@ -71,12 +82,9 @@ if __name__ == "__main__":
     print(df)
     
     # Save the benchmark to a file. Notice that only the methods_ids are saved.
-    my_benchmark.save_to_file(filename = 'results/last_benchmark_detection')
-    # results_interpreter = ResultsInterpreter(my_benchmark)
-    # results_interpreter.save_report()
-    # output_string = results_interpreter.get_table_means()
-    # with open('RESULTS.md', 'w') as f:
-    #     f.write(output_string)
-
+    my_benchmark.save_to_file(filename = 'results/last_benchmark')
+    results_interpreter = ResultsInterpreter(my_benchmark)
+    results_interpreter.save_report()
+    results_interpreter.get_summary_plots(size=(3,2))
 
   
