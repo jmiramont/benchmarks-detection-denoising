0174893bfd5342e3623d7720b8e139a9fea5399c
Juan Manuel <JuanMiram@users.noreply.github.com>
2022-04-14 17:56:18 +0200

    Update method_contours.py

--- src/methods/method_contours.py
+++ src/methods/method_contours.py
@@ -24,5 +24,5 @@ class NewMethod(MethodTemplate):
 
     # def get_parameters(self):            # Use it to parametrize your method.
     #   return [{'q': 0.95}, ]    
-        # return [{'q': i} for i in (0.99, 0.95, 0.9)]
+        # return [{'q': i} for i in (0.99, 0.9, 0.9)]
         
