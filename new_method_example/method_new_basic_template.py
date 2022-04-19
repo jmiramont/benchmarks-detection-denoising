23445b07208ed7effdb3524b6d211091f396898b
jmiramont <jmiramont@uner.edu.ar>
2022-04-19 11:01:31 +0200

    Updating README

--- new_method_example/method_new_basic_template.py
+++ new_method_example/method_new_basic_template.py
@@ -1,39 +1,32 @@
+""" A template file to add new method in the benchmark.
+
+
+Remark: Make sure that this file starts with "method_".
 """
 
-"""
-
-from methods.MethodTemplate import MethodTemplate
-# You must import the MethodTemplate abstract class.
-
-"""
-|
-|
+""" First section ----------------------------------------------------------------------
 | Import here all the modules you need.
 | Remark: Make sure that neither of those modules starts with "method_".
-|
-|
 """
+from methods.MethodTemplate import MethodTemplate # Import the template!
 
-""" 
-|
-|
+
+""" Second section ---------------------------------------------------------------------
 | Put here all the functions that your method uses.
-| Remark: Make sure that this file starts with "method_".
-|
+| 
 | def a_function_of_my_method(signal,params):
 |   ...
-|
-|
 """
 
-""" Create here a new class that will encapsulate your method.
-This class should inherit the abstract class MethodTemplate.
-By doing this, you must then implement the class method: 
+""" Third section ----------------------------------------------------------------------
+| Create here a new class that will encapsulate your method.
+| This class should inherit the abstract class MethodTemplate.
+| You must then implement the class function: 
 
 def method(self, signal, params)
 
-which should receive the signals and any parameters
-that you desire to pass to your method.You can use this file as an example.
+| which should receive the signals and any parameters that you desire to pass to your
+| method.
 """
 
 class NewMethod(MethodTemplate):
