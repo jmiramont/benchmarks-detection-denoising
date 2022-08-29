1fa3880ec34e37b33b076de22853d8aebc9fe530
jmiramont <jmiramont@uner.edu.ar>
2022-08-29 17:50:14 +0200

    Updating conf.

--- run_this_benchmark_denoising.py
+++ run_this_benchmark_denoising.py
@@ -63,8 +63,8 @@ if __name__ == "__main__":
     # Save the benchmark to a file. Notice that only the methods_ids are saved.
     my_benchmark.save_to_file(filename = 'results/last_benchmark')
     results_interpreter = ResultsInterpreter(my_benchmark)
-    results_interpreter.save_csv_files()
+    # results_interpreter.save_csv_files()
     results_interpreter.save_report()
-    results_interpreter.get_summary_plots(size=(3,2))
+    # results_interpreter.get_summary_plots(size=(3,2))
 
   
