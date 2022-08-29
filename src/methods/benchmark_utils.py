be063af3b0b8b6000fa46996a85fa05592849673
jmiramont <jmiramont@uner.edu.ar>
2022-08-29 15:52:13 +0200

    Updating...

--- src/methods/benchmark_utils.py
+++ src/methods/benchmark_utils.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-import matlab.engine
+# import matlab.engine
 from typeguard import is_typeddict
 import numpy as np
 # import sys
@@ -34,29 +34,29 @@ class MethodTemplate(ABC):
     def get_parameters(self):
         return (((),{}),) 
 
-class MatlabInterface():
-    def __init__(self,matlab_function_name):
-        self.matlab_function_name = matlab_function_name
-        self.eng = matlab.engine.start_matlab()
-        self.eng.eval("addpath('src/methods')")
+# class MatlabInterface():
+#     def __init__(self,matlab_function_name):
+#         self.matlab_function_name = matlab_function_name
+#         self.eng = matlab.engine.start_matlab()
+#         self.eng.eval("addpath('src/methods')")
 
-    def matlab_function(self,signal,*params):
-        all_params = list((signal,*params))
-        params = self.pre_parameters(*all_params)
-        fun_handler = getattr(self.eng, self.matlab_function_name)
-        return np.array(fun_handler(*params)[0].toarray())
+#     def matlab_function(self,signal,*params):
+#         all_params = list((signal,*params))
+#         params = self.pre_parameters(*all_params)
+#         fun_handler = getattr(self.eng, self.matlab_function_name)
+#         return np.array(fun_handler(*params)[0].toarray())
         
-    def pre_parameters(self, *params):
-        params_matlab = list()
-        for param in params:
-            if isinstance(param,np.ndarray):
-                params_matlab.append(matlab.double(vector=param.tolist()))
-            if isinstance(param,list) or isinstance(param,tuple):
-                params_matlab.append(matlab.double(vector=list(param)))
-            if isinstance(param,float):
-                params_matlab.append(matlab.double(param))
-            if isinstance(param,int):
-                params_matlab.append(matlab.double(float(param)))
+#     def pre_parameters(self, *params):
+#         params_matlab = list()
+#         for param in params:
+#             if isinstance(param,np.ndarray):
+#                 params_matlab.append(matlab.double(vector=param.tolist()))
+#             if isinstance(param,list) or isinstance(param,tuple):
+#                 params_matlab.append(matlab.double(vector=list(param)))
+#             if isinstance(param,float):
+#                 params_matlab.append(matlab.double(param))
+#             if isinstance(param,int):
+#                 params_matlab.append(matlab.double(float(param)))
                 
-        return params_matlab    
+#         return params_matlab    
 
