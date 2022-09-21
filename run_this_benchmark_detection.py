33c6e0be2ba95aed5d5247db19dec2c1489c7086
jmiramont <juan.miramont@uner.edu.ar>
2022-09-21 18:36:54 +0200

    Adding more detection methods

--- run_this_benchmark_detection.py
+++ run_this_benchmark_detection.py
@@ -1,11 +1,12 @@
 if __name__ == "__main__":
+    # from unittest import result
     import importlib
     from methods import *
     from benchmark_demo.benchmark_utils import MethodTemplate as MethodTemplate
     import time
     import inspect
+    
     # Collects the methods in the folder/ module "methods" and make a global list of them.
-
     print('Collecting methods to benchmark...')
     modules = dir()
     modules = [mod_name for mod_name in modules if mod_name.startswith('method_')]
@@ -23,15 +24,17 @@ if __name__ == "__main__":
                 print(method_name)
                 list_of_methods.append(method_class())
 
+
     from benchmark_demo.Benchmark import *
     import numpy as np
     from benchmark_demo.ResultsInterpreter import ResultsInterpreter
     import yaml
 
+
     dictionary_of_methods = dict()
     dictionary_of_parameters = dict()
 
-    # Select only those methods for detection.
+    # Select only methods for denoising.
     for method_instance in list_of_methods:
         if method_instance.task == 'detection':
             method_id = method_instance.id
@@ -42,24 +45,34 @@ if __name__ == "__main__":
     # Load parameters from configuration file.
     with open("config_detection.yaml", "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-
+    
     config['methods'] = dictionary_of_methods
     config['parameters'] = dictionary_of_parameters
-    config['task'] = 'detection' 
-    np.random.seed(0) 
-    my_benchmark = Benchmark(**config)        
+    config['task'] = 'detection'
 
+    if 'add_new_methods' in config.keys():
+        if config['add_new_methods']:
+            # config.pop('add_new_methods')
+            filename = 'results\last_benchmark_detection'
+            with open(filename + '.pkl', 'rb') as f:
+                benchmark = pickle.load(f)
+            benchmark.add_new_method(config['methods'],config['parameters']) 
+    else:
+        benchmark = Benchmark(**config)    
+
+    np.random.seed(0)
     start = time.time()
-    my_results = my_benchmark.run_test() # Run the test. my_results is a nested dictionary with the results for each of the variables of the simulation.
-    end= time.time()
+    my_results = benchmark.run_test() # Run the test. my_results is a nested dictionary with the results for each of the variables of the simulation.
+    end = time.time()
     print("The time of execution:", end-start)
-    df = my_benchmark.get_results_as_df() # This formats the results on a DataFrame
+    df = benchmark.get_results_as_df() # This formats the results on a DataFrame
     print(df)
     
     # Save the benchmark to a file. Notice that only the methods_ids are saved.
-    my_benchmark.save_to_file(filename = 'results/last_benchmark_detection')
-    results_interpreter = ResultsInterpreter(my_benchmark)
-    # results_interpreter.save_report()
+    benchmark.save_to_file(filename = 'results/last_benchmark_detection')
+    results_interpreter = ResultsInterpreter(benchmark)
+    # results_interpreter.save_csv_files()
+    results_interpreter.save_report()
     # results_interpreter.get_summary_plots(size=(3,2))
 
   
