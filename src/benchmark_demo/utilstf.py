b2f48c484b6205ae15b7208578a667a36dc680a5
jmiramont <juan.miramont@uner.edu.ar>
2022-12-16 16:47:52 +0100

    Changes in stft

--- src/benchmark_demo/utilstf.py
+++ src/benchmark_demo/utilstf.py
@@ -12,10 +12,19 @@ Those functions are:
 
 import numpy as np
 import scipy.signal as sg
+from scipy.fft import fft, ifft
 from math import factorial
 from numpy import complex128, dtype, pi as pi
+def get_gauss_window(Nfft,L,prec=1e-6):
+    l=np.floor(np.sqrt(-Nfft*np.log(prec)/pi))+1
+    N = 2*l+1
+    t0 = l+1
+    tmt0=np.arange(0,N)-t0
+    g = np.exp(-(tmt0/L)**2 * pi)    
+    g=g/np.linalg.norm(g)
+    return g
 
-def get_round_window(Nfft):
+def get_round_window(Nfft, prec = 1e-6):
     """ Generates a round Gaussian window, i.e. same essential support in time and 
     frequency: g(n) = exp(-pi*(n/T)^2) for computing the Short-Time Fourier Transform.
     
@@ -27,54 +36,113 @@ def get_round_window(Nfft):
         T (float): The scale of the Gaussian window (T = sqrt(Nfft))
     """
     # analysis window
-    g = sg.gaussian(Nfft, np.sqrt((Nfft)/2/np.pi))
-    g = g/np.sqrt(np.sum(g**2))
-    T = np.sqrt(Nfft)
-    return g, T
+    L=np.sqrt(Nfft)
+    l=np.floor(np.sqrt(-Nfft*np.log(prec)/pi))+1
+ 
+    N = 2*l+1
+    t0 = l+1
+    tmt0=np.arange(0,N)-t0
+    g = np.exp(-(tmt0/L)**2 * pi)    
+    g=g/np.linalg.norm(g)
+    return g, L
 
+def get_stft(x,window=None,t=None,Nfft=None):
+    xrow = len(x)
 
-def get_stft(signal, window = None):
-    """ Compute the STFT of the signal. Signal is padded with zeros.
-    The outputs corresponds to the STFT with the regular size and also the
-    zero padded version. The signal is zero padded to alleviate border effects.
+    if t is None:
+        t = np.arange(0,xrow)
 
-    Args:
-        signal (ndarray): The signal to analyse.
-        window (ndarray, optional): The window to use. If None, uses a rounded Gaussian
-        window. Defaults to None.
+    if Nfft is None:
+        Nfft = 2*xrow
 
-    Returns:
-        stft(ndarray): Returns de stft of the signal.
-        stft_padded(ndarray): Returns the stft of the zero-padded signal.
-        Npad(int): Number of zeros padded on each side of the signal.
-    """
-    
-    N = np.max(signal.shape)
     if window is None:
-        Nfft = N
-        window, _ = get_round_window(Nfft)
+        window = get_round_window(Nfft)
 
-    Npad = N//2
-    Nfft = len(window)
-    if signal.dtype == complex128:
-        signal_pad = np.zeros(N+2*Npad, dtype=complex128)
-    else:
-        signal_pad = np.zeros(N+2*Npad)
+    tcol = len(t)
+    hlength=np.floor(Nfft/4)
+    hlength=int(hlength+1-np.remainder(hlength,2))
 
-    # signal_pad = np.zeros(N+2*Npad)
-    signal_pad[Npad:Npad+N] = signal
-
-    # computing STFT
-    _, _, stft_padded = sg.stft(signal_pad, window=window, nperseg=Nfft, noverlap = Nfft-1)
+    hrow=len(window)
     
-    # if signal.dtype == complex128:
-    #     stft_padded = stft_padded[0:Nfft//2+1,:]
+    assert np.remainder(hrow,2) == 1
+
+    Lh=(hrow-1)//2
+    tfr= np.zeros((Nfft,tcol))   
+    for icol in range(0,tcol):
+        ti= t[icol]; 
+        tau=np.arange(-np.min([np.round(Nfft/2),Lh,ti]),np.min([np.round(Nfft/2),Lh,xrow-ti])).astype(int)
+        indices= np.remainder(Nfft+tau,Nfft).astype(int); 
+        tfr[indices,icol]=x[ti+tau]*np.conj(window[Lh+tau])/np.linalg.norm(window[Lh+tau])
+    
+    tfr=fft(tfr, axis=0) 
+    return tfr
+
+def get_istft(tfr,window=None,t=None):
+    
+    N,NbPoints = tfr.shape
+    tcol = len(t)
+    hrow = len(window) 
+    Lh=(hrow-1)//2
+    window=window/np.linalg.norm(window)
+    tfr=ifft(tfr,axis=0)
+
+    x=np.zeros((tcol,))
+    for icol in range(0,tcol):
+        valuestj=np.arange(np.max([1,icol-N/2,icol-Lh]),np.min([tcol,icol+N/2,icol+Lh])).astype(int)
+        for tj in valuestj:
+            tau=icol-tj 
+            indices= np.remainder(N+tau,N).astype(int)
+            x[icol]=x[icol]+tfr[indices,tj]*window[Lh+tau]
         
-    stft = stft_padded[:,Npad:Npad+N]
-    return stft, stft_padded, Npad
+        x[icol]=x[icol]/np.sum(np.abs(window[Lh+icol-valuestj])**2)
+    return x
 
 
-def get_spectrogram(signal,window=None):
+# def get_stft(signal, window = None, overlap = None):
+#     """ Compute the STFT of the signal. Signal is padded with zeros.
+#     The outputs corresponds to the STFT with the regular size and also the
+#     zero padded version. The signal is zero padded to alleviate border effects.
+
+#     Args:
+#         signal (ndarray): The signal to analyse.
+#         window (ndarray, optional): The window to use. If None, uses a rounded Gaussian
+#         window. Defaults to None.
+
+#     Returns:
+#         stft(ndarray): Returns de stft of the signal.
+#         stft_padded(ndarray): Returns the stft of the zero-padded signal.
+#         Npad(int): Number of zeros padded on each side of the signal.
+#     """
+    
+#     N = np.max(signal.shape)
+#     if window is None:
+#         window, _ = get_round_window(N)
+
+#     Npad = N//2 #0 #int(np.sqrt(N))
+#     Nfft = len(window)
+    
+#     if overlap is None:
+#         overlap = Nfft-1
+    
+#     if signal.dtype == complex128:
+#         signal_pad = np.zeros(N+2*Npad, dtype=complex128)
+#     else:
+#         signal_pad = np.zeros(N+2*Npad)
+
+#     # signal_pad = np.zeros(N+2*Npad)
+#     signal_pad[Npad:Npad+N] = signal
+
+#     # computing STFT
+#     _, _, stft_padded = sg.stft(signal_pad, window=window, nperseg=Nfft, noverlap = overlap)
+    
+#     # if signal.dtype == complex128:
+#     #     stft_padded = stft_padded[0:Nfft//2+1,:]
+        
+#     stft = stft_padded[:,Npad:Npad+N]
+#     return stft, stft_padded, Npad
+
+
+def get_spectrogram(signal,window=None,Nfft=None,t=None,onesided=True):
     """
     Get the round spectrogram of the signal computed with a given window. 
     
@@ -89,16 +157,28 @@ def get_spectrogram(signal,window=None):
     """
 
     N = np.max(signal.shape)
-    if window is None:
+    if Nfft is None:
         Nfft = 2*N
+
+    if window is None:
         window, _ = get_round_window(Nfft)
 
-    stft, stft_padded, Npad = get_stft(signal, window)
-    S = np.abs(stft)**2
-    return S, stft, stft_padded, Npad
+    if t is None:
+        t = np.arange(0,N)
+        
+    stft=get_stft(signal,window=window,t=t,Nfft=Nfft)
+
+    if onesided:
+        S = np.abs(stft[0:Nfft//2+1,:])**2
+    else:
+        S = np.abs(stft)**2                
+    return S, stft
 
 
 def find_zeros_of_spectrogram(S):
+    aux_S = np.zeros((S.shape[0]+2,S.shape[1]+2))+np.Inf
+    aux_S[1:-1,1:-1] = S
+    S = aux_S
     aux_ceros = ((S <= np.roll(S,  1, 0)) &
             (S <= np.roll(S, -1, 0)) &
             (S <= np.roll(S,  1, 1)) &
@@ -110,8 +190,8 @@ def find_zeros_of_spectrogram(S):
             )
     [y, x] = np.where(aux_ceros==True)
     pos = np.zeros((len(x), 2)) # Position of zeros in norm. coords.
-    pos[:, 0] = y
-    pos[:, 1] = x
+    pos[:, 0] = y-1
+    pos[:, 1] = x-1
     return pos
 
 
@@ -153,7 +233,8 @@ def reconstruct_signal(hull_d, stft):
     return mask, xr, t 
 
 
-def reconstruct_signal_2(mask, stft, Npad, Nfft=None):
+
+def reconstruct_signal_2(mask, stft, Npad, Nfft=None, window=None, overlap=None):
     """Reconstruction using a mask given as parameter
 
     Args:
@@ -169,19 +250,34 @@ def reconstruct_signal_2(mask, stft, Npad, Nfft=None):
     Ni = mask.shape[1]
     if Nfft is None:
         Nfft = Ni
-        
-    # reconstruction
-    g = sg.gaussian(Nfft, np.sqrt((Nfft)/2/np.pi))
-    g = g/g.sum()
+    
+    if window is None:
+        window = sg.gaussian(Nfft, np.sqrt((Nfft)/2/np.pi))
+        window = window/window.sum()
+    
+    Nfft = len(window)
+    
+    if overlap is None:
+        overlap=Nfft-1
+
+
+    # reconstruction        
     mask_aux = np.zeros(stft.shape)
     mask_aux[:,Npad:Npad+Ni] = mask
     # t, xorigin = sg.istft(stft, window=g,  nperseg=Nfft, noverlap=Nfft-1)
-    t, xr = sg.istft(mask_aux*stft, window=g, nperseg=Nfft, noverlap = Nfft-1)
+    t, xr = sg.istft(mask_aux*stft, window=window, nperseg=Nfft, noverlap = overlap)
     xr = xr[Npad:Npad+Ni]
     return xr, t
 
+def reconstruct_signal_3(mask, stft, window=None):
+    mask_complete = np.zeros_like(stft, dtype=bool)
+    mask_complete[0:mask.shape[0],:] = mask
+    mask_complete[mask.shape[0]::,:] = mask[-2:0:-1,:]
+    xr = get_istft(stft*mask_complete,window=window,t=np.arange(0,stft.shape[1]))
+    return xr
 
-# def extr2minth(M,th=1e-14):
+
+# def find_zeros_of_spectrogram(M,th=1e-14):
 #     """ Finds the local minima of the spectrogram matrix M.
 
 #     Args:
@@ -200,7 +296,10 @@ def reconstruct_signal_2(mask, stft, Npad, Nfft=None):
 #             Mid_Mid[c, r] = (np.min(T) == T[1, 1]) * (np.min(T) > th)
 #             #Mid_Mid[c, r] = (np.min(T) == T[1, 1])
 #     x, y = np.where(Mid_Mid)
-#     return x, y
+#     pos = np.zeros((len(x), 2)) # Position of zeros in norm. coords.
+#     pos[:, 0] = x
+#     pos[:, 1] = y
+#     return pos
 
 
 def snr_comparison(x,x_hat):
@@ -366,21 +465,16 @@ def hermite_fun(N, q, t=None, T=None, return_all = False):
     else:
         return hfunc[-1], hfunc
         
-def sigmerge(x1, noise, ratio, return_noise=False, tmin=0, tmax=None):
-        # Get signal parameters.
-        N = len(x1)
-        if tmax is None:
-            tmax = N
-            
-        sig = 1e-6*np.random.randn(noise.shape[0],N)
+def sigmerge(x1, noise, ratio, return_noise=False):
+    # Get signal parameters.
+    N = len(x1)
+    ex1=np.mean(np.abs(x1)**2)
+    ex2=np.mean(np.abs(noise)**2)
+    h=np.sqrt(ex1/(ex2*10**(ratio/10)))
+    scaled_noise = noise*h
+    sig=x1+scaled_noise
 
-        ex1=np.mean(np.abs(x1[tmin:tmax])**2)
-        ex2=np.mean(np.abs(noise)**2, axis=1)
-        h=np.sqrt(ex1/(ex2*10**(ratio/10)))
-        scaled_noise = noise*h.reshape((noise.shape[0],1))
-        sig[:,tmin:tmax]=x1[tmin:tmax]+scaled_noise
-
-        if return_noise:
-            return sig, scaled_noise
-        else:
-            return sig
+    if return_noise:
+        return sig, scaled_noise
+    else:
+        return sig
